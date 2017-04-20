8a75044f7a8b82b15f08c7af41f71a595ee0ca83
OPISTO\ville.varis <ville.vm.varis@gmail.com>
2017-04-20 13:45:20 +0300

    lisätty kuutio ja sen skripti

---
+++ TestiProjekti/VersioHallintaRojekti/Assets/Scripts/CubeMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMover : MonoBehaviour {
+
+    public float speed = 19;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position += new Vector3(1, 2, 3) * Time.deltaTime * speed;
+		
+	}
+}
